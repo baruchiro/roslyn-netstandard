4bfd0456870a6d48f0aa16d5008072ccbbcfdd6d
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-11-12 09:01:12 +0200

    Works with netcoreapp2.1

---
+++ CoreExe/Program.cs
@@ -0,0 +1,13 @@
+﻿using Library;
+using System;
+
+namespace CoreExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompilerLibrary.Compile();
+        }
+    }
+}
--- Library/CompilerLibrary.cs
+++ Library/CompilerLibrary.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -30,29 +31,39 @@ namespace Library
 
             options = options.WithAssemblyIdentityComparer(AssemblyIdentityComparer.Default);
 
-            var references = new MetadataReference[]
+            var references = new List<MetadataReference>
             {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                // MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
             };
+
+            if (AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES") is string trustedAssemblies)
+            {
+                Console.WriteLine(trustedAssemblies);
+                references.AddRange(
+                    trustedAssemblies.Split(Path.PathSeparator).Select(path =>
+                        MetadataReference.CreateFromFile(path))
+                );
+            }
 
             var compilation = CSharpCompilation.Create("DummyAssembly", options: options, references: references)
                 .AddSyntaxTrees(testTree);
 
-            var stream = new MemoryStream();
-            var compilationResult = compilation.Emit(stream);
+            using (var stream = new MemoryStream())
+            {
+                var compilationResult = compilation.Emit(stream);
 
-            if (!compilationResult.Success)
-            {
-                Console.WriteLine(string.Join("\n", compilationResult.Diagnostics.Select(d => d.GetMessage())));
-            }
-            else
-            {
-                stream.Seek(0, SeekOrigin.Begin);
-                var assembly = Assembly.Load(stream.ToArray());
-                var type = assembly.GetType("test.Power");
-                var power = Activator.CreateInstance(type);
-                type.InvokeMember("power", BindingFlags.Default | BindingFlags.InvokeMethod, null, power, new object[] { 2 });
+                if (!compilationResult.Success)
+                {
+                    Console.WriteLine(string.Join("\n", compilationResult.Diagnostics.Select(d => d.GetMessage())));
+                }
+                else
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                    var assembly = Assembly.Load(stream.ToArray());
+                    var type = assembly.GetType("test.Power");
+                    var power = Activator.CreateInstance(type);
+                    type.InvokeMember("power", BindingFlags.Default | BindingFlags.InvokeMethod, null, power, new object[] { 2 });
+                }
             }
         }
     }
